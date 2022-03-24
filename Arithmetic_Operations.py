d20a7160915e90f3957d0fd344031b11c539a40b
sherlocksameer7 <arijitsameers@gmail.com>
2022-03-24 14:56:29 +0530

    Assignment 1

---
+++ Arithmetic_Operations.py
@@ -0,0 +1,44 @@
+import unittest
+
+def add(x, y):
+    return x+y
+
+def sub(x, y):
+    return x-y
+
+def mul(x, y):
+    return x*y
+
+def div(x, y):
+    return x/y
+
+class Addition_with_2Num(unittest.TestCase):
+
+    def test_case_add(self):
+        a = 6
+        b = 8
+        c = add(a, b)
+        self.assertEqual(c, a+b)
+
+
+    def test_case_sub(self):
+        a = 14
+        b = 7
+        c = sub(a, b)
+        self.assertEqual(a-b, c)
+
+
+    def test_case_mul(self):
+        a = 7
+        b = 2
+        c = mul(a, b)
+        self.assertEqual(c, a*b)
+
+    def test_case_div(self):
+        a = 70
+        b = 10
+        c = div(a, b)
+        self.assertEqual(a/b, c)
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ Operations_Conditions.py
@@ -0,0 +1,63 @@
+import unittest
+
+def EvenorOdd(x):
+    if x%2 == 0:
+        return "even"
+    else:
+        return "odd"
+
+
+
+def Divisibleby7(x):
+    if x % 7 == 0:
+        return True
+    else:
+        return False
+
+
+
+def PrimeorNot(x):
+    if x > 1:
+        for i in range(2, int(x / 2) + 1):
+            if (x % i) == 0:
+                break
+        else:
+            return True
+    else:
+        return False
+
+
+
+class testcasesapp(unittest.TestCase):
+
+    def test_case_evenorodd(self):
+        result = EvenorOdd(2)
+        self.assertEqual("even", result)
+
+    def test_case_evenorodd1(self):
+        result = EvenorOdd(15)
+        self.assertEqual("odd", result)
+
+
+
+    def test_case_divisibleby7_1(self):
+        result = Divisibleby7(14)
+        self.assertTrue(result)
+
+    def test_case_divisibleby7_2(self):
+        result = Divisibleby7(4)
+        self.assertFalse(result)
+
+
+
+    def test_case_primeorNot(self):
+        result = PrimeorNot(2)
+        self.assertTrue(result)
+
+    def test_case_primeorNot2(self):
+        result = PrimeorNot(1)
+        self.assertFalse(result)
+
+
+if __name__ == "__main__":
+    unittest.main()
